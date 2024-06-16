76e801bcdb28c119061524f3e5b0efc6c1c7d8ae
Wederson Fagundes <wedymf@hotmail.com>
2024-06-16 19:08:31 -0300

    feat: add get all organization projects

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -28,6 +28,7 @@ import { updateOrganization } from './routes/orgs/update-organizations'
 import { createProject } from './routes/projects/create-project'
 import { deleteProject } from './routes/projects/delete-project'
 import { getProject } from './routes/projects/get-project'
+import { getProjects } from './routes/projects/get-projects'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -84,6 +85,7 @@ app.register(transferOrganization)
 app.register(createProject)
 app.register(deleteProject)
 app.register(getProject)
+app.register(getProjects)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')

991c9dd307c883a21f75496f2430d11635b62ba8
Wederson Fagundes <wedymf@hotmail.com>
2024-06-16 02:45:44 -0300

    feat: add reset password

---
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -0,0 +1,50 @@
+import { hash } from 'bcryptjs'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function resetPassword(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/password/reset',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Reset password with token and new password',
+        body: z.object({
+          code: z.string(),
+          password: z.string().min(6),
+        }),
+        response: {
+          204: z.null(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { code, password } = request.body
+
+      const tokenFromCode = await prisma.token.findUnique({
+        where: { id: code },
+      })
+
+      if (!tokenFromCode) {
+        // We don't want people to know if user really exists
+        return reply.status(204).send()
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.update({
+        where: {
+          id: tokenFromCode.userId,
+        },
+        data: {
+          passwordHash,
+        },
+      })
+
+      return reply.status(204).send()
+    },
+  )
+}
